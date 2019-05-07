4017bc92433ba4b4943d0a4a15ed2feda7e1fced
Michael Menzel <michael.menzel@mni.thm.de>
2019-05-07 15:56:11 +0200

    gui filter imp

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -1,6 +1,5 @@
 package gui;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import filter.GroupKMers;
 import logo.LogoOld;
 import profile.ProfileLib;
@@ -16,8 +15,6 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Gui extends Frame {
 
@@ -28,6 +25,8 @@ public class Gui extends Frame {
     private JTextPane seq;
     private StyledDocument doc;
     private boolean colorall = true;
+    private Map<String, List<String>> groupedKmers;
+    private String hash;
 
     public int getBasematch() {
         return basematch;
@@ -56,6 +55,7 @@ public class Gui extends Frame {
         doc = new DefaultStyledDocument();
         seq = new JTextPane(doc);
         seq.setFont(new Font("Verdana", Font.PLAIN, 25));
+        seq.setEditable(false);
         images.add(seq);
 
 
@@ -94,11 +94,14 @@ public class Gui extends Frame {
                 update();
         });
 
+        JLabel cutoff_slider_label = new JLabel("Score cutoff:");
+        buttons.add(cutoff_slider_label);
         buttons.add(cutoff_slider);
 
         // Slider
         // Slider
 
+        JLabel basematch_slider_label = new JLabel("Basematch:");
         JSlider basematch_slider = new JSlider(1,8,basematch);
         basematch_slider.setMinorTickSpacing(1);
         basematch_slider.setMajorTickSpacing(2);
@@ -112,47 +115,16 @@ public class Gui extends Frame {
                 update();
         });
 
+        buttons.add(basematch_slider_label);
         buttons.add(basematch_slider);
-
-        JButton continue_btn = new JButton("continue");
-        buttons.add(continue_btn);
-
-
-
-        continue_btn.addActionListener(e -> {
-
-            f.dispose();
-
-            System.out.println("Cluster kmers that overlap with at least x bases. Filter kmers with a score less than " + score_cutoff);
-            Map<String, List<String>> groupedKmers = GroupKMers.groupKMers(scores, basematch, score_cutoff);
-
-            groupedKmers.keySet().forEach(base -> {
-                System.out.println(base.toUpperCase());
-                System.out.println(groupedKmers.get(base).size());
-            });
-
-            System.out.println("Create sequence logos from clustered kmers:");
-
-            groupedKmers.keySet().forEach(base -> {
-                LogoOld logo = new LogoOld(base, scores);
-
-                System.out.println("=====");
-                System.out.println("file: ");
-                System.out.println("-----");
-                System.out.println(base);
-                System.out.println(logo);
-                System.out.println("-----");
-                groupedKmers.get(base).stream().limit(500).forEach(System.out::println);
-                System.out.println("=====");
-            });
-        });
 
 
 
         // Input expected
-
+        JLabel inputlabel = new JLabel("Filter kmers:");
         JTextField input = new JTextField();
         input.setMaximumSize(new Dimension(100,input.getPreferredSize().height));
+        buttons.add(inputlabel);
         buttons.add(input);
 
         input.getDocument().addDocumentListener(new DocumentListener() {
@@ -184,13 +156,49 @@ public class Gui extends Frame {
 
         buttons.add(new Box.Filler(new Dimension(5,5),new Dimension(10,10),new Dimension(1000,1000)));
 
+
+        JButton continue_btn = new JButton("continue");
+        buttons.add(continue_btn);
+
+        continue_btn.addActionListener(e -> {
+            f.dispose();
+
+            System.out.println("Cluster kmers that overlap with at least x bases. Filter kmers with a score less than " + score_cutoff);
+            Map<String, List<String>> groupedKmers = GroupKMers.groupKMers(scores, basematch, score_cutoff);
+
+            groupedKmers.keySet().forEach(base -> {
+                System.out.println(base.toUpperCase());
+                System.out.println(groupedKmers.get(base).size());
+            });
+
+            System.out.println("Create sequence logos from clustered kmers:");
+
+            groupedKmers.keySet().forEach(base -> {
+                LogoOld logo = new LogoOld(base, scores);
+
+                System.out.println("=====");
+                System.out.println("file: ");
+                System.out.println("-----");
+                System.out.println(base);
+                System.out.println(logo);
+                System.out.println("-----");
+                groupedKmers.get(base).stream().limit(500).forEach(System.out::println);
+                System.out.println("=====");
+            });
+        });
+
+
         // Slider
         f.setVisible(true);
         update();
     }
 
     private void update(){
-        Map<String, List<String>> groupedKmers = GroupKMers.groupKMers(scores, basematch, score_cutoff);
+
+        if(!(score_cutoff +  ":" + basematch).equals(hash)) {
+            groupedKmers = GroupKMers.groupKMers(scores, basematch, score_cutoff);
+            hash = score_cutoff + ":" + basematch;
+        }
 
         StringBuilder alltext = new StringBuilder();
 
@@ -218,12 +226,10 @@ public class Gui extends Frame {
 
         } else for (char letter: chars){
         //shift
-        for(int j = 1; j < tmp.length; j++){
-            tmp[j-1] = tmp[j];
-        }
+        if (tmp.length - 1 >= 0) System.arraycopy(tmp, 1, tmp, 0, tmp.length - 1);
         tmp[tmp.length-1] = letter;
 
-        if(Arrays.equals(tmp, filter.toCharArray())){
+        if(Arrays.equals(tmp, filter.toCharArray()) || Arrays.equals(tmp, new StringBuilder(filter).reverse().toString().toCharArray()) ){
             for(int i = pos - filter.length() + 1; i < pos +1; i++){
                 char l2 =  chars[i];
 
