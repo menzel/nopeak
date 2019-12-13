05eb5733365a38bdb8f93306e48bf379f3342eab
Michael Menzel <michael.menzel@mni.thm.de>
2019-12-13 11:45:18 +0100

    Refactoring

--- src/main/java/main/LogoHelper.java
+++ src/main/java/main/LogoHelper.java
@@ -12,11 +12,11 @@ class LogoHelper {
 
     static void logo(String path_sample, int fraglen, boolean gui, String kmerOutputFile) {
 
-        Profile profile_c = new Profile(path_sample);
+        Profile profile = new Profile(path_sample);
 
-        Map<String, List<Integer>> profiles_control = profile_c.getResult();
+        Map<String, List<Integer>> profiles_control = profile.getResult();
 
-        Scoring scoring = new Scoring(profiles_control, fraglen, profile_c.getReadcount());
+        Scoring scoring = new Scoring(profiles_control, fraglen, profile.getReadcount());
         List<Score> scores = scoring.getScores();
 
         if (kmerOutputFile != null)
@@ -32,23 +32,20 @@ class LogoHelper {
 
     static void logo(String path_control, String path_sample, int fraglen, boolean gui, String kmerOutputFile) {
 
-            ////////////////////
-            // Get Profiles for both sample and control from files
-            ////////////////////
+        ////////////////////
+        // Get Profiles for both sample and control from files
+        ////////////////////
 
-        //System.out.println("[" + (System.currentTimeMillis() - startTime) + "] Reading sample profiles file " + path_sample);
-            Profile profile_s = new Profile(path_sample);
+        Profile profile_s = new Profile(path_sample);
 
 
-        //System.out.println("[" + (System.currentTimeMillis() - startTime) + "] Reading control profiles file " + path_control);
-            Profile profile_c = new Profile(path_control);
+        Profile profile_c = new Profile(path_control);
 
-            Map<String, List<Integer>> profiles_sample = profile_s.getResult();
-            Map<String, List<Integer>> profiles_control = profile_c.getResult();
+        Map<String, List<Integer>> profiles_sample = profile_s.getResult();
+        Map<String, List<Integer>> profiles_control = profile_c.getResult();
 
-        //System.out.println("[" + (System.currentTimeMillis() - startTime) + "] Scoring kmers by curves");
-        Scoring scoring = new Scoring(profiles_sample, profiles_control, fraglen);
-            List<Score> scores = scoring.getScores();
+        Scoring scoring = new Scoring(profiles_sample, profiles_control, fraglen, profile_s.getReadcount());
+        List<Score> scores = scoring.getScores();
 
         if (kmerOutputFile != null)
             scoring.writeToFile(kmerOutputFile);
