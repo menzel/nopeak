4b030d5e29d713a975c474c0ed662b04367f2f34
Michael Menzel <michael.menzel@mni.thm.de>
2021-06-10 11:39:49 +0200

    Issue with logo package name resolved

--- src/main/java/logo/Logo.java
+++ src/main/java/logo/Logo.java
@@ -14,7 +14,7 @@
 // 
 // You should have received a copy of the GNU General Public License
 // along with NoPeak.  If not, see <https://www.gnu.org/licenses/>.  
-package java.logo;
+package logo;
 
 import score.Score;
 
